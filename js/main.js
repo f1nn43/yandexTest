7f902bd20fdaf85b62343efa6b50723fa94a66f5
f1nn43 <petrovegor615@gmail.com>
2024-10-25 18:43:05 +0300

    Steps Section + Adaptive

--- js/main.js
+++ js/main.js
@@ -1,21 +1,36 @@
-const swiper = new Swiper('.swiper5', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+const section5Carousel = document.querySelector('.section5__carousel');
+
+const corousel5Func = () => {
+  const width = document.body.clientWidth;
+  if(width < 1000){
+    section5Carousel.classList.add('swiper5')
+    section5Carousel.classList.add('swiper')
+    const swiper = new Swiper('.swiper5', {
+      // Optional parameters
+      direction: 'horizontal',
+    
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    
+      // And if we need scrollbar
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    });
+  } else {
+    section5Carousel.classList.remove('swiper5')
+  }
+}
+
+window.onload = corousel5Func()
+window.addEventListener('resize',(e) => {
+  corousel5Func()
+})
